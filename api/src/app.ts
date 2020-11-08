a383636e4355aa36a2a93c269308bb9163f2c320
Anders Fibiger <anders.afj@gmail.com>
2020-11-08 16:28:57 +0100

    add express

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,1 +1,10 @@
-console.log('hej');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log('Listening on port: ', PORT));
